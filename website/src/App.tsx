fae0821fb35a62d4761f77d23a329574b192e9cd
Lukáš Helcl <luky.helcl@gmail.com>
2025-09-17 14:32:45 +0200

    feat: add routing for menu and page switching

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,14 +1,26 @@
 import './App.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './ui-components/layout/Layout'
+import Menu from './ui-components/menu/menu'
+import Dashboard from './pages/dashboard/Dashboard'
+import MyDevices from './pages/my-devices/MyDevices';
 
 
 function App() {
 
   return (
-    <>
-    <Layout/>
-    </>
+    <Router>
+      <Layout/>
+      <div className='body-section'>
+      <Menu/>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='my-device' element={<MyDevices/>}/>
+      </Routes>
+      </div>
+    
+    </Router>
   )
-}
+};
 
-export default App
+export default App;
